7d9d62e8bd346f090b2d9525382417d8e1feaf94
saty0212 <sschouhan.cse@mnit.ac.in>
2026-02-07 09:05:43 +0530

    updated print

--- App.java
+++ App.java
@@ -1,6 +1,6 @@
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello, World");//changed the print statement
+        System.out.println("Hello, git 1");//changed the print statement
         System.out.println("This is a demo for branching, we have added 1 feature.");
     }
 }
