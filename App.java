8bb7b1829a185ef1f640efafa99857909d34ebf0
saty0212 <sschouhan.cse@mnit.ac.in>
2026-02-07 08:41:18 +0530

    added one feature

--- App.java
+++ App.java
@@ -1,5 +1,6 @@
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello, Git!");
+        System.out.println("This is a demo for branching, we have added 1 feature.");
     }
 }
